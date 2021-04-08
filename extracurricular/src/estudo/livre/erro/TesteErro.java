0571e745a4ae67812b7a57db038e71a783cd6647
Alexsandro Cristiano <acg.alexsandro@hotmail.com>
2021-04-07 23:52:04 -0300

    estudando tratamento de erro e excecao basico

--- extracurricular/src/estudo/livre/erro/TesteErro.java
+++ extracurricular/src/estudo/livre/erro/TesteErro.java
@@ -3,8 +3,28 @@ package estudo.livre.erro;
 public class TesteErro {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		  System.out.println("inicio do main");
+	      metodo1();
+	      System.out.println("fim do main");
+	  }
 
-	}
+	  static void metodo1() {
+	      System.out.println("inicio do metodo1");
+	      metodo2();
+	      System.out.println("fim do metodo1");
+	  }
 
+	  static void metodo2() {
+	      System.out.println("inicio do metodo2");
+	      int[] vet = new int[5];
+	 //     try {
+		      for(int i=0 ; i < 6 ; i++) {
+		    	  vet[i] = i+2;
+		      }
+	   //   }
+	     // catch(Exception e) {
+	    //	  e.printStackTrace();
+	      //}
+	      System.out.println("fim do metodo2");
+	  }
 }
---
+++ extracurricular/src/estudo/livre/erro/TesteErro2.java
@@ -0,0 +1,11 @@
+package estudo.livre.erro;
+
+public class TesteErro2 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		
+		int r = a/0;
+		System.out.println(r);
+	}
+}
