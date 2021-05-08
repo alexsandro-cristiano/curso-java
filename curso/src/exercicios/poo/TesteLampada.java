3d862e77d145ba16c09016a557ec31075569d1e6
Alexsandro Cristiano <75186371+alexsandro-cristiano@users.noreply.github.com>
2021-05-08 09:04:12 -0300

    exercicio lampada

---
+++ curso/src/exercicios/poo/TesteLampada.java
@@ -0,0 +1,16 @@
+package exercicios.poo;
+
+public class TesteLampada {
+
+	public static void main(String[] args) {
+		
+		Lampada lamp = new Lampada();
+		
+		lamp.ligar();
+		lamp.ligar();
+		
+		lamp.desligar();
+
+	}
+
+}
