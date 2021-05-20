8b9b1d925bd00227a7e61ed8b4282a917d0c089f
Alexsandro Cristiano <75186371+alexsandro-cristiano@users.noreply.github.com>
2021-05-20 10:30:04 -0300

    exercicio secao 13 - Order

---
+++ curso/src/javacompleto/exercicios/secao/treze/exercicio/OrderStatus.java
@@ -0,0 +1,6 @@
+package javacompleto.exercicios.secao.treze.exercicio;
+
+public enum OrderStatus {
+
+	PENDING_PAYMENT, PROCESSING, SHIPPED, DELIVERED;
+}
---
+++ curso/src/javacompleto/exercicios/secao/treze/exercicio/Programa.java
@@ -0,0 +1,5 @@
+package javacompleto.exercicios.secao.treze.exercicio;
+
+public class Programa {
+
+}
