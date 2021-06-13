52bd5209d4f14dd8c544e4d286f578ae46496ec3
Alexsandro Cristiano <75186371+alexsandro-cristiano@users.noreply.github.com>
2021-06-13 10:24:06 -0300

    Add Exercicios de Introducao a POO em Java

---
+++ Introducao a POO/src/Lampada.java
@@ -0,0 +1,46 @@
+
+public class Lampada {
+	// Exercicio proposto no curso de Java Basico da Loiane Groner
+	private String cor;
+	private String potencia;
+	private Boolean lampadaLigada = false;
+
+	public Lampada() {
+
+	}
+
+	public Lampada(String cor, String potencia) {
+		this.cor = cor;
+		this.potencia = potencia;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public String getPotencia() {
+		return potencia;
+	}
+
+	public void ligar() {
+		if (verificarEstadoLampada()) {
+			this.lampadaLigada = true;
+			System.out.print("Lampada Ligada\n");
+		} else {
+			System.out.print("Lampada já esta Ligada\n");
+		}
+	}
+
+	public void desligar() {
+		if (!verificarEstadoLampada()) {
+			this.lampadaLigada = false;
+			System.out.print("Lampada Desligada\n");
+		} else {
+			System.out.print("Lampada já esta Desligada\n");
+		}
+	}
+
+	private Boolean verificarEstadoLampada() {
+		return (lampadaLigada == false) ? true : false;
+	}
+}
---
+++ Introducao a POO/src/TesteLampada.java
@@ -0,0 +1,14 @@
+
+public class TesteLampada {
+
+	public static void main(String[] args) {
+		Lampada lamp = new Lampada();
+
+		lamp.ligar();
+		lamp.ligar();
+
+		lamp.desligar();
+
+	}
+
+}
