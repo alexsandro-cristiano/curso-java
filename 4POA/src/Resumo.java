b9328cd5f8cb65330a3a66d94a9c69d986a44e9a
Alexsandro Cristiano <acg.alexsandro@hotmail.com>
2021-09-06 11:55:04 -0300

    4 periodo faculdade

---
+++ 4POA/src/Resumo.java
@@ -0,0 +1,77 @@
+
+public class Resumo {
+
+	public static void main(String[] args) {
+		/*
+		 * Fazer um resumo de cada um dos Itens:
+
+			-Classe x Objeto-Ambiente de Programação
+			Classe = fabrica de objetos
+			Obejtos = instancias da fabibrica
+			Amabiente de Programação = IDE para desenvolver os codigos.
+			
+			-Visibilidade
+			Forma de declarar uma variavel dentro da classe e/ou programa, compoem o encapsulamento
+			
+			-Métodos
+			Sçao funções das classes.
+			
+			-Herança
+			É a forma de reutilizar codigo, herdando as caracterias da classe mae.
+			 
+			-Interface
+			é um "contrato" para que uma classe possa utiliza todos os metodos já definidos e implementar da forma que lhe atendente
+			
+			-Encapsulamento
+			um dos pilares da POO. Visa "esconder" ou não os metodos e atributos de uma classe
+			
+			-Polimorfismo
+			é um dos pilares da POO. Podemos fazer com que o objeto responda de formas diferentes a uma mensagem recebida
+			
+			-Pacotes
+			maneira de organizar as classes do sistema
+			
+			-Método construtor
+			é um metodo que auxilia na instanciação do objeto
+			
+			-Métodos gets e sets
+			metodos especiais que compõem a ideia de encapsulamento. utilizado para pegar ou inserir uma informação no objeto
+			
+			-Classe abstrata
+			uma classe generica que nao pode ser instanciada, ou seja, não tera objetos apartir dela.
+			
+			-Modificador final
+			modificador para criar constantes. quando utilizado na declaração da classe ela nao pode ser herdada
+			
+			-Reescrita
+			forma de reescrever um metodo já existente, da forma que melhor lhe atende
+			
+			-Sobrecarga
+			ter o metodo de mesmo nomes mas com parametros diferentes e saida diferente
+			
+			-Modificador static
+			modificador que permite que o atributo ou metodo seja acessado sem a necessidade de criar um objeto
+			
+			-Coleções
+			é a parte do java de implementar estruturas de dados
+			
+			-Enum
+			maneira de criar constantes pre definidas
+			
+			-Varags
+			uma maneira de passar n argumentos para um metodo
+			
+			-Introdução as Anotações.
+			forma de usar metadata
+			
+			-Javadoc
+			documentação do sistema em forma de web page
+			
+			-Introdução Lambda e Method Reference
+			expresoem de programação funcional.
+			
+			*/
+
+	}
+
+}
