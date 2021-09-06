0b4b10ecdedae26068042a69476cada7d420935f
Alexsandro Cristiano <acg.alexsandro@hotmail.com>
2021-09-06 12:19:28 -0300

    Seller entity class

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
+		
 		System.out.println(obj);
 
+		Seller seller = new Seller();
+		
+		System.out.println(seller);
 	}
 
 }
